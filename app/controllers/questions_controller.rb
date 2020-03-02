ffc8cd80e4077e485189cf65940836933fa9007b
kevin <kraynaud@gmail.com>
2020-03-02 13:05:23 +0100

    stupid coaching emplemented

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @answer = ''
+    @answer = if params[:question] == 'I am going to work'
+                ' Great!'
+              elsif params[:question] == '?'
+                'Silly question, get dressed and go to work!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
+    @answer
+  end
+end
